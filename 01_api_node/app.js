ca28ec95ee87a1416539ac705e0b5f8e0679bf56
maopos <mauricioposadamurillo@gmail.com>
2022-05-14 18:16:36 -0500

    first commit

---
+++ 01_api_node/app.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log("");
+  console.log("=======================");
+  console.log("✅ Server on port:", port);
+});
